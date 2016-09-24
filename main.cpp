34cc949886f1f42882114e2f4e6b744defe4472c
super-chalupa <eduardorobles@Eduardos-MacBook-Pro.local>
2016-09-23 17:49:12 -0700

    Added a new function

--- main.cpp
+++ main.cpp
@@ -5,9 +5,18 @@
 #include <iostream>
 using namespace std;
 
+//Function prototypes
+int luckyNum();
+
 int main()
 {
     cout<<"this is a simple program\n";
-
+    cout<<"\n Hey your lucky number is "<<luckyNum<<endl;
     return 0;
+}
+
+int luckyNum()
+{
+    return 5;
+
 }
